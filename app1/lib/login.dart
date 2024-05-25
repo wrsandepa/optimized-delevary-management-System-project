c5bfd483c92da78d0bf99a2db7509f025da1a22b
wrsandepa <ranjulasandeepa@gmail.com>
2024-05-25 15:43:44 +0530

    update

--- app1/lib/login.dart
+++ app1/lib/login.dart
@@ -11,54 +11,77 @@ class Loging extends StatelessWidget {
           'hello',
         ),
         backgroundColor: Colors.blue,
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/singup');
+            },
+            style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 199, 208, 214)),
+            child: const Text(
+              'Singin!',
+              style: TextStyle(color: Colors.blue, fontSize: 12),
+            ),
+          ),
+        ],
       ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Container(
-          margin:
-              const EdgeInsets.fromLTRB(1000 / 100, 5 / 100, 4, 18000 / 100),
-          // color: Colors.red,
-          child: const Text(
-            'Login your account',
-            style: TextStyle(
-                color: Colors.blue, fontSize: 20, fontStyle: FontStyle.italic),
-          ),
-        ),
-        TextFormField(
-          decoration: InputDecoration(
-              label: const Text(
-                'Username',
-                style: TextStyle(
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+            margin:
+                const EdgeInsets.fromLTRB(1000 / 100, 5 / 100, 4, 18000 / 100),
+            // color: Colors.red,
+            child: const Text(
+              'Login your account',
+              style: TextStyle(
                   color: Colors.blue,
-                ),
-              ),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(25))),
-          obscureText: true, // This will obscure the text with star marks
-        ),
-        const SizedBox(height: 5),
-        TextFormField(
-          decoration: InputDecoration(
-              label: const Text(
-                'Password',
-                style: TextStyle(
-                  color: Colors.blue,
-                ),
-              ),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(25))),
-          obscureText: true, // This will obscure the text with star marks
-        ),
-        const SizedBox(height: 5),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/singup');
-          },
-          child: const Text(
-            'already haven`t account',
-            style: TextStyle(color: Colors.blue, fontSize: 15),
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic),
+            ),
           ),
-        ),
-      ]),
+          SizedBox(
+            width: 400,
+            child: TextFormField(
+              decoration: InputDecoration(
+                  label: const Text(
+                    'Username',
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25))),
+              obscureText: true, // This will obscure the text with star marks
+            ),
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: 400,
+            child: TextFormField(
+              decoration: InputDecoration(
+                  label: const Text(
+                    'Password',
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25))),
+              obscureText: true, // This will obscure the text with star marks
+            ),
+          ),
+          const SizedBox(height: 50),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/singup');
+            },
+            child: const Text(
+              'already haven`t account',
+              style: TextStyle(color: Colors.blue, fontSize: 15),
+            ),
+          ),
+        ]),
+      ),
       backgroundColor: Colors.white,
     );
   }
--- app1/lib/singup.dart
+++ app1/lib/singup.dart
@@ -17,82 +17,101 @@ class Singupage extends StatelessWidget {
         backgroundColor: Colors.blue,
       ),
       backgroundColor: Colors.white,
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Container(
-          margin: const EdgeInsets.fromLTRB(2.2, 5, 4, 15000 / 100),
-          // color: Colors.red,
-          child: const Text(
-            'welocom! sing up to continue',
-            style: TextStyle(
-                color: Colors.blue, fontSize: 20, fontStyle: FontStyle.italic),
-          ),
-        ),
-        TextFormField(
-          decoration: InputDecoration(
-              label: const Text(
-                'Username',
-                style: TextStyle(color: Colors.blue),
-              ),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(25))),
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        TextFormField(
-          controller: _password, // Assign the controller
-          decoration: InputDecoration(
-              label: const Text(
-                'password',
-                style: TextStyle(
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+            margin: const EdgeInsets.fromLTRB(2.2, 5, 4, 15000 / 100),
+            // color: Colors.red,
+            child: const Text(
+              'welocom! sing up to continue',
+              style: TextStyle(
                   color: Colors.blue,
-                ),
-              ),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(25))),
-          obscureText: true, // This will obscure the text with star marks
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        TextFormField(
-          decoration: InputDecoration(
-              label: const Text(
-                'Email',
-                style: TextStyle(color: Colors.blue),
-              ),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(25))),
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        TextFormField(
-          decoration: InputDecoration(
-              label: const Text(
-                'Name',
-                style: TextStyle(color: Colors.blue),
-              ),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(25))),
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        ElevatedButton(
-          onPressed: () {
-            String username = _username.text;
-            String name = _name.text;
-            String password = _password.text;
-            String email = _email.text;
-            Navigator.pushNamed(context, '/login'); //navigater mark left top
-          },
-          child: const Text(
-            'singup',
-            style: TextStyle(color: Colors.blue, fontSize: 15),
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic),
+            ),
           ),
-        ),
-      ]),
+          SizedBox(
+            width: 400,
+            child: TextFormField(
+              controller: _username,
+              decoration: InputDecoration(
+                  label: const Text(
+                    'Username',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25))),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: 400,
+            child: TextFormField(
+              controller: _password, // Assign the controller
+              decoration: InputDecoration(
+                  label: const Text(
+                    'password',
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25))),
+              obscureText: true, // This will obscure the text with star marks
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: 400,
+            child: TextFormField(
+              controller: _email,
+              decoration: InputDecoration(
+                  label: const Text(
+                    'Email',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25))),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: 400,
+            child: TextFormField(
+              controller: _name,
+              decoration: InputDecoration(
+                  label: const Text(
+                    'Name',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25))),
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              String username = _username.text;
+              String name = _name.text;
+              String password = _password.text;
+              String email = _email.text;
+              Navigator.pushNamed(context, '/login'); //navigater mark left top
+            },
+            child: const Text(
+              'singup',
+              style: TextStyle(color: Colors.blue, fontSize: 15),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
