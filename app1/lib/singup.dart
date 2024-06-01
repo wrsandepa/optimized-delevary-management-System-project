93865e9b3235185afed470a993bdb41a035b2e85
wrsandepa <ranjulasandeepa@gmail.com>
2024-06-01 16:24:02 +0530

    firebase config

--- app1/lib/singup.dart
+++ app1/lib/singup.dart
@@ -10,7 +10,7 @@ class Singupage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: AppBar( 
           title: const Text(
             'hello',
           ),
